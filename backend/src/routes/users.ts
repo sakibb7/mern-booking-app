02eb3f95ec1f36ab22126ba8287f1716c1f80f14
sakibb7 <sakibb076@gmail.com>
2024-06-30 10:37:42 +0600

    updated

---
+++ backend/src/routes/users.ts
@@ -0,0 +1,59 @@
+import express, { Request, Response } from "express";
+import User from "../models/user";
+import jwt from "jsonwebtoken";
+import { check, validationResult } from "express-validator";
+
+const router = express.Router();
+
+router.post(
+  "/registration",
+  [
+    check("firstName", "First name is required").isString(),
+    check("lastName", "Last name is required").isString(),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password is required").isLength({
+      min: 6,
+    }),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array() });
+    }
+
+    try {
+      let user = await User.findOne({
+        email: req.body.email,
+      });
+
+      if (user) {
+        return res.status(400).json({ message: "User Already Exist" });
+      }
+
+      user = new User(req.body);
+      await user.save();
+
+      const token = jwt.sign(
+        { userId: user.id },
+        process.env.JWT_SECRET_KEY as string,
+        { expiresIn: "1h" }
+      );
+
+      res.cookie("auth_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 3600000,
+      });
+
+      console.log(process.env.NODE_ENV);
+
+      return res.sendStatus(200);
+    } catch (error) {
+      console.log(error);
+
+      res.status(500).send({ message: "Bad Request" });
+    }
+  }
+);
+
+export default router;
